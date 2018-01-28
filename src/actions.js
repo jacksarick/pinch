f0e702b919fb858b188bc12d2a7b62f5d6c54df7
jacksarick <jacksarick@gmail.com>
2018-01-28 04:30:10 -0400

    filter works

--- src/actions.js
+++ src/actions.js
@@ -1,10 +1,24 @@
-function fuzzy_search(argument) {
-	console.log(argument);
+function fuzzy_search(search) {
+	console.log(search);
 }
 
-function filter(argument){
-	console.log(argument);
+function filter() {
+	// Pull all filters into array and get the values of checked ones
+	filters = [...document.getElementsByClassName("filter")].filter(x => x.checked).map(x => x.value);
+
+	// Pull all items
+	items = [...document.getElementsByClassName("entry")]
+	
+	for (var i = items.length - 1; i >= 0; i--) {
+		// default to hidden
+		items[i].style.display = "none";
+		
+		if (filters.includes(items[i].id)) {
+			items[i].style.display = "table-row";
+		}
+	}
 }
+
 
 function update(key, value){
 	console.log({key: value});
@@ -15,12 +29,12 @@ function new_record(){
 }
 
 function format(name, type, value) {
-	return `<tr id='${type}'>
+	return `<tr class="entry" id="${type}" value="${name}">
 				<td>${name}</td>
 				<td>${type}</td>
 				<td>
-					<input type='number' value='${value}' onchange='update(${name}, [${type}, ${value}]);'>
-					<button onclick='remove(${name});'>Remove</button>
+					<input type="number" value="${value}" onchange="update(${name}, [${type}, ${value}]);">
+					<button onclick="remove(${name});">Remove</button>
 				</td>
 			</tr>`
 }
