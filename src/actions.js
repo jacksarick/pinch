7b78c158f35a59c3aa235eb4cbdc65c48b83c2d2
jacksarick <jacksarick@gmail.com>
2018-01-28 13:41:22 -0400

    Commented what little code there is

--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,4 @@
+// Parse data from localStorage
 function local_storage() {
 	return JSON.parse(localStorage.db);
 }
@@ -23,23 +24,26 @@ function filter() {
 	}
 }
 
-
+// Read-modify-write data
 function update(key, value){
 	var data = local_storage();
 	data[key] = value;
 	localStorage.db = JSON.stringify(data);
 }
 
+// Read-modify-write data
 function remove(key){
 	var data = local_storage();
 	delete data[key];
 	localStorage.db = JSON.stringify(data);
 }
 
+// Short hand for update() with data from DOM
 function new_record(){
 	update($("#new-name").value(), [document.querySelector('input[name=new-type]:checked').value, $("#new-value").value()]);
 }
 
+// Format data as table row
 function format(name, type, value) {
 	return `<tr class="entry" id="${type}" value="${name}">
 				<td>${name}</td>
